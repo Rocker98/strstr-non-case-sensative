393f8ac9135d21e75d48715eebda19b5f75c8335
Volodymyr Petruniak <y.petrunyak@gmail.com>
2018-09-16 23:37:09 +0300

    library function strstr() without case-sensity

---
+++ main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+#define MAX 5
+
+
+
+int main(int argc, char *argv[])
+{
+	char first[MAX],second[MAX];
+	bool caseSensetive=0;
+	bool trig=1;
+
+	puts("Enter the string");
+	scanf("%5s",&first);
+	puts("Enter the string");
+	scanf("%5s",&second);	
+	int i;
+	for(i=0;i<MAX;i++)
+	{
+		if(isupper(first[i])&&islower(second[i]))
+		{
+			if((first[i]+32)==second[i])
+			{
+				caseSensetive=1;
+			}
+			else 
+			{
+				trig=0;
+				break;
+			}
+		}
+		else if(islower(first[i])&&isupper(second[i]))
+		{
+			if((first[i]-32)==second[i])
+			{
+				caseSensetive=1;
+			}
+			else
+		 	{
+				trig=0;
+				break;
+			}
+		}
+	}
+	
+	if((strstr(first,second))==0&&(!caseSensetive||(!trig)))	
+	puts("Not identical");
+	else if(strstr(first,second)||caseSensetive&&trig)
+	puts("They are same");
+
+	
+
+	return 0;
+}
